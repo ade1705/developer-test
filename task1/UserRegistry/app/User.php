13949a70180a33898b603566ce8d9f046d0a4f94
adeadedoja <damdey@yahoo.com>
2018-08-30 13:36:35 +0200

    Edited factory and setup users seed

--- task1/UserRegistry/app/User.php
+++ task1/UserRegistry/app/User.php
@@ -17,13 +17,4 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'surname', 'email', 'position',
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 }
--- task1/UserRegistry/database/migrations/2014_10_12_000000_create_users_table.php
+++ task1/UserRegistry/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,42 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
             $table->string('position');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
-}
-
-            $table->rememberToken();
             $table->timestamps();
         });
     }
